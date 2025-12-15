55759f6327ff7a16935768cd0e90cea8e77577fc
asif256000 <asif256000@gmail.com>
2025-12-15 17:18:33 -0500

    Add convenience exports for the data_generation package

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+"""Convenience exports for the data_generation package."""
+
+from .data_generation.tree_sequence_generator import TreeSequenceGenerator, TreeSequenceResult
+
+__all__ = ["TreeSequenceGenerator", "TreeSequenceResult"]
